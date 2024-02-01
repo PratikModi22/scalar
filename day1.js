c608eff57f9425d2a7b67d1cc74c6fa046f1c7ff
PratikModi22 <142490832+PratikModi22@users.noreply.github.com>
2024-02-01 22:04:03 +0530

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,16 @@
+// day1
+const fs = require('fs');
+function readFileContent(filePath){
+    fs.readFile(filePath,function(err,data){
+        if(err){
+            console.log("Error Reading file:",err.message);
+        }
+        else
+        {
+            console.log(data.toString());
+        }
+    });
+}
+readFileContent('test-files/file1.txt');
+readFileContent('test-files/empty-file.txt');
+readFileContent('test-files/nonexistent-file.txt');
